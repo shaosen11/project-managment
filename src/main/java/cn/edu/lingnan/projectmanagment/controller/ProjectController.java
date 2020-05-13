25dcb26968e02c2f7ad5240d424cad41a04739b5
shaosen11 <56765775+shaosen11@users.noreply.github.com>
2020-05-13 21:48:54 +0800

    主页面显示

--- src/main/java/cn/edu/lingnan/projectmanagment/controller/ProjectController.java
+++ src/main/java/cn/edu/lingnan/projectmanagment/controller/ProjectController.java
@@ -43,44 +43,30 @@ public class ProjectController {
 
     //查询一条项目信息
     @GetMapping("/get_by_id")
-    public String getById(Integer id,Model model){
-        System.out.println("查询项目id:"+id);
-        Projects projects=projectService.getById(id);
-        if (projects != null){
-            model.addAttribute("projectmsg",projects);
+    public String getById(Integer id, Model model) {
+        System.out.println("查询项目id:" + id);
+        Projects projects = projectService.getById(id);
+        if (projects != null) {
+            model.addAttribute("projectmsg", projects);
             System.out.println(projects);
             return "echarts_test";
-        }else {
-            model.addAttribute("projectmsg","此项目不存在");
+        } else {
+            model.addAttribute("projectmsg", "此项目不存在");
             return "failure";
         }
     }
 
     //查询所有项目信息
     @GetMapping("/projects_list")
-    public String projectList(Model model){
+    public String projectList(Model model) {
         List<Projects> list = projectService.getProjectList();
-        model.addAttribute("projectlist",list);
-        System.out.println("查询所有项目"+list);
+        model.addAttribute("projectlist", list);
+        System.out.println("查询所有项目" + list);
         return "tables/projectlist";
     }
 
     @GetMapping("/projects_view")
-    public String projectView(Integer projectId, Model model){
-        System.out.println("项目id " + projectId);
-        model.addAttribute("projectId",projectId);
-        //获取进度条
-        Integer data1 = projectsFunctionService.countProjectFunctionByProjectId(projectId);
-        if(data1 == 0){
-            model.addAttribute("projectFunctionData",0);
-            System.out.println("data1="+data1);
-        }else{
-            Integer data2 = projectsFunctionService.countByProjectIdAndStatus(projectId,2);
-            Integer data3 = projectsFunctionService.countByProjectIdAndStatus(projectId,3);
-            Integer projectFunctionData = ((data2+data3)*100)/data1;
-            model.addAttribute("projectFunctionData",projectFunctionData);
-            System.out.println("data1="+data1+" data2="+data2+" data3="+data3+" projectFunctionData="+projectFunctionData);
-        }
+    public String projectView() {
         return "project/projectview";
     }
 
@@ -88,19 +74,19 @@ public class ProjectController {
     //添加项目
     @ResponseBody
     @PostMapping("/add_project")
-    public Integer addProject(Projects project){
-        System.out.println("待添加项目："+project+" ChargeUserId="+project.getChargeUserId());
-        if (userService.findById(project.getChargeUserId()) == null){
+    public Integer addProject(Projects project) {
+        System.out.println("待添加项目：" + project + " ChargeUserId=" + project.getChargeUserId());
+        if (userService.findById(project.getChargeUserId()) == null) {
             System.out.println("该项目负责人不存在");
             return 1;
-        }else{
+        } else {
             project.setSchedule("未开始");
-            System.out.println("添加项目："+project);
+            System.out.println("添加项目：" + project);
             Boolean flag = projectService.addProject(project);
-            if (flag){
+            if (flag) {
                 System.out.println("添加项目成功！");
                 return 2;
-            }else{
+            } else {
                 System.out.println("添加项目失败！");
                 return 3;
             }
@@ -111,12 +97,12 @@ public class ProjectController {
     @Transactional
     @ResponseBody
     @PostMapping("/del_project")
-    public Boolean deleteProject(Integer id){
-        try{
+    public Boolean deleteProject(Integer id) {
+        try {
             projectUserService.deleteProjectUserByProjectsId(id);
             projectsFunctionService.deleteProjectFunctionByProjectsId(id);
             projectService.deleteProject(id);
-        }catch (Exception e){
+        } catch (Exception e) {
             TransactionAspectSupport.currentTransactionStatus().setRollbackOnly();
             System.out.println("事务回滚，删除失败！");
             return false;
@@ -127,18 +113,18 @@ public class ProjectController {
     //修改项目信息
     @ResponseBody
     @PostMapping("/edit_project")
-    public Integer editProject(Projects project){
+    public Integer editProject(Projects project) {
         MyUserDetails myUserDetails = userService.findById(project.getChargeUserId());
-        if (myUserDetails == null){
+        if (myUserDetails == null) {
             System.out.println("该项目负责人不存在");
             return 1;
-        }else{
-            System.out.println("editproject项目："+project);
+        } else {
+            System.out.println("editproject项目：" + project);
             Boolean flag = projectService.editProject(project);
-            if(flag){
+            if (flag) {
                 System.out.println("修改项目信息成功");
                 return 2;
-            }else{
+            } else {
                 System.out.println("修改项目信息失败");
                 return 3;
             }
@@ -147,43 +133,43 @@ public class ProjectController {
 
     //查询所有注销项目信息
     @GetMapping("/del_project_list")
-    public String deletedProjectList(Model model){
+    public String deletedProjectList(Model model) {
         List<Projects> list = projectService.getDelProjectList();
-        model.addAttribute("delprojectlist",list);
-        System.out.println("查询所有已注销用户"+list);
+        model.addAttribute("delprojectlist", list);
+        System.out.println("查询所有已注销用户" + list);
         return "deleted/deleteproject";
     }
 
     //还原用户项目
     @ResponseBody
     @PostMapping("/reduction_project")
-    public Boolean reductionProject(Integer id){
-        Boolean flag =  projectService.reductionProject(id);
-        System.out.println("还原项目:"+id+flag);
-        if(flag){
+    public Boolean reductionProject(Integer id) {
+        Boolean flag = projectService.reductionProject(id);
+        System.out.println("还原项目:" + id + flag);
+        if (flag) {
             return true;
-        }else {
+        } else {
             return false;
         }
     }
 
     @GetMapping("/newprojectview")
-    public String newProjectView(HttpServletRequest request, Model model){
+    public String newProjectView(HttpServletRequest request, Model model) {
         //1.从HttpServletRequest中获取SecurityContextImpl对象
         SecurityContextImpl securityContextImpl = (SecurityContextImpl) request.getSession().getAttribute("SPRING_SECURITY_CONTEXT");
         //2.从SecurityContextImpl中获取Authentication对象
         Authentication authentication = securityContextImpl.getAuthentication();
-        MyUserDetails userDetails = (MyUserDetails)authentication.getPrincipal();
+        MyUserDetails userDetails = (MyUserDetails) authentication.getPrincipal();
         System.out.println(userDetails);
         //我所有项目
         List<Myprojects> myprojectsList = userService.getMyProjects(userDetails.getId());
-        System.out.println("userid:"+userDetails.getId()+" 我的项目："+myprojectsList);
-        model.addAttribute("myprojects",myprojectsList);
+        System.out.println("userid:" + userDetails.getId() + " 我的项目：" + myprojectsList);
+        model.addAttribute("myprojects", myprojectsList);
         return "project/newprojectview";
     }
 
     @PostMapping("/projects")
-    public String newprojects(Projects projects){
+    public String newprojects(Projects projects) {
         //设置项目初始状态
         projects.setSchedule("未开始");
         //插入数据库
@@ -197,7 +183,7 @@ public class ProjectController {
         ProjectsCodeLine projectsCodeLine = new ProjectsCodeLine();
         projectsCodeLine.setProjectsId(projects.getId());
         projectsCodeLineService.insert(projectsCodeLine);
-        Map<String,Object> pathMap = new HashMap<>();
+        Map<String, Object> pathMap = new HashMap<>();
         pathMap.put("projectId", projects.getId());
         pathMap.put("userId", projects.getChargeUserId());
         String pathString = PathUtil.pathUtil(pathMap);
@@ -206,8 +192,19 @@ public class ProjectController {
 
     @GetMapping("/project")
     @ResponseBody
-    public  Projects getProjectByProjectId(Integer projectId){
+    public Projects getProjectByProjectId(Integer projectId) {
         return projectService.getById(projectId);
+    }
+
+    @GetMapping("/todayProjectsAndWeekProjects")
+    @ResponseBody
+    public Object getTodayProjectsAndWeekProjects(){
+        List todayProjectsList = projectService.getTodayProject();
+        List weekProjectsList = projectService.getWeekProject();
+        Map<String, List> projectMap = new HashMap<>();
+        projectMap.put("todayProjectsList", todayProjectsList);
+        projectMap.put("weekProjectsList", weekProjectsList);
+        return projectMap;
     }
 }
 
--- src/main/java/cn/edu/lingnan/projectmanagment/controller/pageController.java
+++ src/main/java/cn/edu/lingnan/projectmanagment/controller/pageController.java
@@ -46,9 +46,9 @@ public class pageController {
         try {
             // 获取总条目数
             Integer count = null;
-            if(userId == 0){
+            if (userId == 0) {
                 count = documentsRecordService.getDocumentsRecordCountByProjectId(projectId);
-            }else {
+            } else {
                 count = documentsRecordService.getDocumentsRecordCountByProjectIdAndUserId(projectId, userId);
             }
             // 计算总页数
@@ -58,14 +58,14 @@ public class pageController {
                 totalPage++;
             }
             // 当页数大于总页数，直接返回
-            if (page > totalPage){
+            if (page > totalPage) {
                 return null;
             }
             // 计算sql需要的起始索引
             int offset = (page - 1) * pageSize;
             // 根据起始索引和页面大小去查询数据
             List<DocumentsRecord> list = null;
-            if(userId == 0){
+            if (userId == 0) {
                 list = documentsRecordService.getDocumentsRecordPageByProjectId(projectId, offset, pageSize);
             } else {
                 list = documentsRecordService.getDocumentsRecordPageByProjectIdAndUserId(projectId, userId, offset, pageSize);
@@ -85,7 +85,7 @@ public class pageController {
         }
     }
 
-    public Map<String, Object> functionPageCom(Integer page, Integer projectId,Integer count){
+    public Map<String, Object> functionPageCom(Integer page, Integer projectId, Integer count) {
         Map<String, Object> map = new HashMap<String, Object>();
         // 每页显示条数
         int pageSize = 5;
@@ -97,7 +97,7 @@ public class pageController {
                 totalPage++;
             }
             // 当页数大于总页数，直接返回
-            if (page > totalPage){
+            if (page > totalPage) {
                 return null;
             }
             // 计算sql需要的起始索引
@@ -106,7 +106,7 @@ public class pageController {
             map.put("page", page);
             map.put("pageSize", pageSize);
             map.put("totalPage", totalPage);
-            map.put("offset",offset);
+            map.put("offset", offset);
             return map;
         } catch (Exception e) {
             System.out.println("获取函数数据失败" + e);
@@ -117,21 +117,21 @@ public class pageController {
     //项目功能--全部功能
     @GetMapping("/allFunctionPage")
     @ResponseBody
-    public ResponseEntity<Map<String, Object>> allFunctionPage(Integer page, Integer projectId,Integer functionStatus) {
-        System.out.println("当前页：" + page+"  当前项目：" + projectId);
+    public ResponseEntity<Map<String, Object>> allFunctionPage(Integer page, Integer projectId, Integer functionStatus) {
+        System.out.println("当前页：" + page + "  当前项目：" + projectId);
         Map<String, Object> map = new HashMap<String, Object>();
         // 每页显示条数
         int pageSize = 5;
         try {
             // 获取总条目数
-            int count = projectsFunctionService.countByProjectIdAndStatus(projectId,functionStatus);
-            map = functionPageCom(page,projectId,count);
+            int count = projectsFunctionService.countByProjectIdAndStatus(projectId, functionStatus);
+            map = functionPageCom(page, projectId, count);
             int offset = (int) map.get("offset");
             // 根据起始索引和页面大小去查询数据
-            List<ProjectsFunction> list = projectsFunctionService.getAllFunctionPage(projectId, offset, pageSize,functionStatus);
+            List<ProjectsFunction> list = projectsFunctionService.getAllFunctionPage(projectId, offset, pageSize, functionStatus);
             // 封装数据，并返回
             map.put("list", list);
-            System.out.println("分页map"+map);
+            System.out.println("分页map" + map);
             return new ResponseEntity<Map<String, Object>>(map, HttpStatus.OK);
         } catch (Exception e) {
             System.out.println("获取分页数据失败" + e);
@@ -143,21 +143,21 @@ public class pageController {
     //项目功能--指派给我的
     @GetMapping("/assignFunctionPage")
     @ResponseBody
-    public ResponseEntity<Map<String, Object>> assignFunctionPage(Integer page, Integer projectId, Integer userId,Integer functionStatus) {
-        System.out.println("assignFunctionPage::当前页：" + page+"  当前项目：" + projectId);
+    public ResponseEntity<Map<String, Object>> assignFunctionPage(Integer page, Integer projectId, Integer userId, Integer functionStatus) {
+        System.out.println("assignFunctionPage::当前页：" + page + "  当前项目：" + projectId);
         Map<String, Object> map = new HashMap<String, Object>();
         // 每页显示条数
         int pageSize = 5;
         try {
             // 获取总条目数
-            int count = projectsFunctionService.countProjectFunctionByProjectIdAndRealizeUserId(projectId,userId,functionStatus);
-            map = functionPageCom(page,projectId,count);
+            int count = projectsFunctionService.countProjectFunctionByProjectIdAndRealizeUserId(projectId, userId, functionStatus);
+            map = functionPageCom(page, projectId, count);
             int offset = (int) map.get("offset");
             // 根据起始索引和页面大小去查询数据
-            List<ProjectsFunction> list = projectsFunctionService.getFunctionByProjectIdAndRealizeUserId(projectId, offset, pageSize,userId,functionStatus);
+            List<ProjectsFunction> list = projectsFunctionService.getFunctionByProjectIdAndRealizeUserId(projectId, offset, pageSize, userId, functionStatus);
             // 封装数据，并返回
             map.put("list", list);
-            System.out.println("分页map"+map);
+            System.out.println("分页map" + map);
             return new ResponseEntity<Map<String, Object>>(map, HttpStatus.OK);
         } catch (Exception e) {
             System.out.println("获取分页数据失败" + e);
@@ -169,21 +169,21 @@ public class pageController {
     //项目功能--我发布的
     @GetMapping("/publishFunctionPage")
     @ResponseBody
-    public ResponseEntity<Map<String, Object>> publishFunctionPage(Integer page, Integer projectId, Integer userId,Integer functionStatus) {
-        System.out.println("publishFunctionPage::当前页：" + page+"  当前项目：" + projectId);
+    public ResponseEntity<Map<String, Object>> publishFunctionPage(Integer page, Integer projectId, Integer userId, Integer functionStatus) {
+        System.out.println("publishFunctionPage::当前页：" + page + "  当前项目：" + projectId);
         Map<String, Object> map = new HashMap<String, Object>();
         // 每页显示条数
         int pageSize = 5;
         try {
             // 获取总条目数
-            int count = projectsFunctionService.countProjectFunctionByProjectIdAndPublishUserId(projectId,userId,functionStatus);
-            map = functionPageCom(page,projectId,count);
+            int count = projectsFunctionService.countProjectFunctionByProjectIdAndPublishUserId(projectId, userId, functionStatus);
+            map = functionPageCom(page, projectId, count);
             int offset = (int) map.get("offset");
             // 根据起始索引和页面大小去查询数据
-            List<ProjectsFunction> list = projectsFunctionService.getFunctionByProjectIdAndPublishUserId(projectId, offset, pageSize,userId,functionStatus);
+            List<ProjectsFunction> list = projectsFunctionService.getFunctionByProjectIdAndPublishUserId(projectId, offset, pageSize, userId, functionStatus);
             // 封装数据，并返回
             map.put("list", list);
-            System.out.println("分页map"+map);
+            System.out.println("分页map" + map);
             return new ResponseEntity<Map<String, Object>>(map, HttpStatus.OK);
         } catch (Exception e) {
             System.out.println("获取分页数据失败" + e);
@@ -195,21 +195,21 @@ public class pageController {
     //项目功能--我参与的
     @GetMapping("/joinFunctionPage")
     @ResponseBody
-    public ResponseEntity<Map<String, Object>> joinFunctionPage(Integer page, Integer projectId, Integer userId,Integer functionStatus) {
-        System.out.println("joinFunctionPage::当前页：" + page+"  当前项目：" + projectId);
+    public ResponseEntity<Map<String, Object>> joinFunctionPage(Integer page, Integer projectId, Integer userId, Integer functionStatus) {
+        System.out.println("joinFunctionPage::当前页：" + page + "  当前项目：" + projectId);
         Map<String, Object> map = new HashMap<String, Object>();
         // 每页显示条数
         int pageSize = 5;
         try {
             // 获取总条目数
-            int count = projectsFunctionService.countProjectFunctionByProjectIdAndUserId(projectId,userId,functionStatus);
-            map = functionPageCom(page,projectId,count);
+            int count = projectsFunctionService.countProjectFunctionByProjectIdAndUserId(projectId, userId, functionStatus);
+            map = functionPageCom(page, projectId, count);
             int offset = (int) map.get("offset");
             // 根据起始索引和页面大小去查询数据
-            List<ProjectsFunction> list = projectsFunctionService.getFunctionByProjectIdAndUserId(projectId, offset, pageSize,userId,functionStatus);
+            List<ProjectsFunction> list = projectsFunctionService.getFunctionByProjectIdAndUserId(projectId, offset, pageSize, userId, functionStatus);
             // 封装数据，并返回
             map.put("list", list);
-            System.out.println("分页map"+map);
+            System.out.println("分页map" + map);
             return new ResponseEntity<Map<String, Object>>(map, HttpStatus.OK);
         } catch (Exception e) {
             System.out.println("获取分页数据失败" + e);
@@ -218,56 +218,60 @@ public class pageController {
         }
     }
 
-    @GetMapping("/projectsRecommendationPage")
-    @ResponseBody
-    public ResponseEntity<Map<String, Object>> projectsRecommendationPage(Integer page, Integer projectId, Integer userId) {
-        System.out.println("joinFunctionPage::当前页：" + page );
-        Map<String, Object> map = new HashMap<String, Object>();
-        // 每页显示条数
-        int pageSize = 5;
-        try {
-            // 获取总条目数
-            int count = projectService.countProjectsRecommendation();
-            // 计算总页数
-            int totalPage = count / pageSize;
-            // 不满一页的数据按一页计算
-            if (count % pageSize != 0) {
-                totalPage++;
-            }
-            // 当页数大于总页数，直接返回
-            if (page > totalPage){
-                return null;
-            }
-            // 计算sql需要的起始索引
-            int offset = (page - 1) * pageSize;
-            // 根据起始索引和页面大小去查询数据
-            List<ProjectsRecommendation> list = projectService.getProject(offset, pageSize);
-            System.out.println("更多项目:::"+list);
-            // 封装数据，并返回
-            map.put("page", page);
-            map.put("pageSize", pageSize);
-            map.put("totalPage", totalPage);
-            map.put("list", list);
-
-            return new ResponseEntity<Map<String, Object>>(map, HttpStatus.OK);
-
-        } catch (Exception e) {
-            System.out.println("获取分页数据失败" + e);
-            return new ResponseEntity<Map<String, Object>>(
-                    HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
+//    @GetMapping("/projectsRecommendationPage")
+//    @ResponseBody
+//    public ResponseEntity<Map<String, Object>> projectsRecommendationPage(Integer page) {
+//        System.out.println("joinFunctionPage::当前页：" + page);
+//        Map<String, Object> map = new HashMap<String, Object>();
+//        // 每页显示条数
+//        int pageSize = 5;
+//        try {
+//            // 获取总条目数
+//            int count = projectService.countProjectsRecommendation();
+//            // 计算总页数
+//            int totalPage = count / pageSize;
+//            // 不满一页的数据按一页计算
+//            if (count % pageSize != 0) {
+//                totalPage++;
+//            }
+//            // 当页数大于总页数，直接返回
+//            if (page > totalPage) {
+//                return null;
+//            }
+//            // 计算sql需要的起始索引
+//            int offset = (page - 1) * pageSize;
+//            // 根据起始索引和页面大小去查询数据
+//            List<ProjectsRecommendation> list = projectService.getProject(offset, pageSize);
+//            System.out.println("更多项目:::" + list);
+//            // 封装数据，并返回
+//            map.put("page", page);
+//            map.put("pageSize", pageSize);
+//            map.put("totalPage", totalPage);
+//            map.put("list", list);
+//
+//            return new ResponseEntity<Map<String, Object>>(map, HttpStatus.OK);
+//
+//        } catch (Exception e) {
+//            System.out.println("获取分页数据失败" + e);
+//            return new ResponseEntity<Map<String, Object>>(
+//                    HttpStatus.INTERNAL_SERVER_ERROR);
+//        }
+//    }
 
     @GetMapping("/getProjectsByTypePage")
     @ResponseBody
     public ResponseEntity<Map<String, Object>> getProjectsByTypePage(Integer page, String projectsType) {
-        System.out.println("getProjectsByTypePage::当前页：" + page+"，type::类型为：" + projectsType );
-        Map<String, Object> map = new HashMap<String, Object>();
+        System.out.println("getProjectsByTypePage::当前页：" + page + "，type::类型为：" + projectsType);
+        Map<String, Object> map = new HashMap<>();
         // 每页显示条数
         int pageSize = 5;
         try {
-            // 获取总条目数
-            int count = projectService.countProjectsNumberByType(projectsType);
+            int count = 0;
+            if ("".equals(projectsType)) {
+                count = projectService.countProjectsRecommendation();
+            } else {
+                count = projectService.countProjectsNumberByType(projectsType);
+            }
             // 计算总页数
             int totalPage = count / pageSize;
             // 不满一页的数据按一页计算
@@ -275,14 +279,18 @@ public class pageController {
                 totalPage++;
             }
             // 当页数大于总页数，直接返回
-            if (page > totalPage){
+            if (page > totalPage) {
                 return null;
             }
             // 计算sql需要的起始索引
             int offset = (page - 1) * pageSize;
-            // 根据起始索引和页面大小去查询数据
-            List<ProjectsRecommendation> list = projectService.getProjectsByType(offset, pageSize,projectsType);
-            System.out.println("指定类型项目:::"+list);
+            List<ProjectsRecommendation> list = null;
+            if ("".equals(projectsType)) {
+                list =  projectService.getProject(offset, pageSize);
+            } else {
+                list = projectService.getProjectsByType(offset, pageSize, projectsType);
+            }
+            System.out.println("指定类型项目:::" + list);
             // 封装数据，并返回
             map.put("page", page);
             map.put("pageSize", pageSize);
