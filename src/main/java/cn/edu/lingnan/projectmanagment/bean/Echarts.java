18d5d29abec2ffbc7041e6318ba27937f9d41ff4
leader <1137363187@qq.com>
2020-06-03 22:58:31 +0800

    上传项目页面

--- src/main/java/cn/edu/lingnan/projectmanagment/bean/Echarts.java
+++ src/main/java/cn/edu/lingnan/projectmanagment/bean/Echarts.java
@@ -7,9 +7,10 @@ public class Echarts {
     private String name;
     private Integer value;
 
-    public Echarts() { }
+    public Echarts() {
+    }
 
-    public Echarts( String name, Integer num) {
+    public Echarts(String name, Integer num) {
         this.name = name;
         this.value = num;
     }
